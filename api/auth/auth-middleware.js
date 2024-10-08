f925432149d18ff3649bf369c78fefb90d581065
Stephen Aranda <aranda.stephen88@gmail.com>
2024-10-08 12:23:14 -0500

    exported middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -84,3 +84,12 @@ const checkForMissingCredentials = (req, res, next) => {
     }
 
 }
+
+
+module.exports = {
+    restricted,
+    checkIfEmailExists,
+    checkIfEmailAlreadyRegistered,
+    checkForMissingCredentials
+
+}
