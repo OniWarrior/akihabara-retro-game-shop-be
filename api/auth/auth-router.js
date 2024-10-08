6cd92f6ec11f728377378128de1e66ab6c804ef9
Stephen Aranda <aranda.stephen88@gmail.com>
2024-10-08 18:17:41 -0500

    created token payload

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,8 @@
 const User = require('../users/user-model')
 const {
     restricted,
-    checkIfEmailExists,
-    checkIfEmailAlreadyRegistered,
+    checkIfUsernameExists,
+    checkIfUsernameAlreadyRegistered,
     checkForMissingCredentials
 } = requre('./auth-middleware')
 
@@ -13,17 +13,19 @@ const router = require('express')
 
 
 // path to login existing user
-router.post('/login', checkForMissingCredentials, checkIfEmailExists, async (req, res, next) => {
+router.post('/login', checkForMissingCredentials, checkIfUsernameExists, async (req, res, next) => {
 
     try {
         // retrieve credentials
-        const { email, password } = req.body
+        const { username, password } = req.body
 
         // find user
-        const user = await User.findByEmail(email)
+        const user = await User.findByUsername(username)
 
         // check the password
         const encryption = bcrypt.compareSync(password, user.password)
+
+
 
 
 
@@ -32,3 +34,22 @@ router.post('/login', checkForMissingCredentials, checkIfEmailExists, async (req
 
     }
 })
+
+
+// make a token for successful login
+const makeToken = (credentials) => {
+
+    // create payload
+    const payload = {
+        username: credentials.username,
+        password: credentials.password
+    }
+
+    // state how long does token lasts
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, JWT_SECRET, options)
+
+}
