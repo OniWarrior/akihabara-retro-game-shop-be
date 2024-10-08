d6764d954fe30ea46dfc7aa5cdcb7b4b8c9ff723
Stephen Aranda <aranda.stephen88@gmail.com>
2024-10-08 11:49:02 -0500

    reject if token not present

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -5,5 +5,12 @@ const User = require('../users/user-model')
 
 //middleware-verifies that json web token is in user's header
 const restricted = (req, res, next) => {
+    // get the token
+    const token = req.headers.authorization
+
+    // check whether or not a token is present
+    if (!token) {
+        res.status(401).json("Token required")
+    }
 
 }
