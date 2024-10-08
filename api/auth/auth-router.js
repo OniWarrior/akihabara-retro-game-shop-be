ae6e0e9a9f5c0508d8cad2c9224c2c40f39a8a1a
Stephen Aranda <aranda.stephen88@gmail.com>
2024-10-08 12:31:25 -0500

    placed empty try catch

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -9,4 +9,15 @@ const {
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = require('./secrets/secret')
+const router = require('express')
 
+
+// path to login existing user
+router.post('/login', checkForMissingCredentials, checkIfEmailExists, async (req, res, next) => {
+
+    try {
+
+    } catch {
+
+    }
+})
