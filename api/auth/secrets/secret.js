847423cb936f305141d996aa5752a21b95d8de55
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-12 19:44:18 -0600

    restart: added proj scaffold

--- api/auth/secrets/secret.js
+++
@@ -1,2 +0,0 @@
-const JWT_SECRET = process.env.JWT_SECRET || 'shhhh' // shhhh is for testing.
-module.exports = JWT_SECRET
--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     production: {
         ...sharedConfig,
-        connection: DATABASE_URL,
+        connection: process.env.DATABASE_URL,
         pool: { min: 2, max: 10 }
     }
 };
