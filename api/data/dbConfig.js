e819b83e1a890ba73276f3ec8172a4784ac554d5
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-12 19:00:02 -0600

    changed file name

--- api/data/dbConfig.js
+++ api/data/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
-const config = require('../../knex.js')
+const config = require('../../knexfile.js')
 const environment = process.env.NODE_ENV
 
 module.exports = knex(config[environment])
