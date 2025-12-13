305b3e5291d1fdbe9f1f9d606f702e84c853902a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-12 21:43:30 -0600

    imported secret

--- api/secrets/secrets.js
+++ api/secrets/secrets.js
@@ -3,3 +3,10 @@
  * File  : secrets.js
  * Desc  : File that imports secrets from the file containing the environmental variables.
  * */
+
+require('dotenv').config()
+
+// import the secret for the session
+const SESSION_SECRET = process.env.SESSION_SECRET || "shhh" // fallback for testing
+
+module.exports = { SESSION_SECRET }
