f5933979caef80d0d616058525cb9559a839b1f3
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-13 19:38:06 -0600

    added dummy manager user

--- api/data/seeds/01-users.js
+++ api/data/seeds/01-users.js
@@ -12,5 +12,10 @@ const users = [
         username: 'hello_there_123',
         password: '741852963!@#',
         user_type: 'Customer'
+    },
+    {
+        username: 'holla_back_girl_123',
+        password: '789456123456',
+        user_type: 'Manager'
     }
 ]
