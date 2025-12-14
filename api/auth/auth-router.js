0cb5e4ba78b7d41e932509536f16fe2dbb51ce60
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-13 23:13:52 -0600

    added failure response to logout

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,3 +3,26 @@
  * File  : auth-router.js
  * Desc  : File that contains the endpoints for login and signup
  * */
+
+const {
+    requiredAuthorization,
+    checkForMissingCreds,
+    checkUsernameExists
+
+} = require('./auth-middleware');
+
+const bcrypt = require('bcrypt');
+const router = require('express').Router();
+
+
+/*
+ * /logout: endpoint to log out a user from a session
+ */
+router.post('/logout', checkForMissingCreds, requiredAuthorization, async (req, res) => {
+    try {
+
+    } catch (err) {
+        // failure response
+        return res.status(500).json({ message: `Server Error: ${err.message}` });
+    }
+})
