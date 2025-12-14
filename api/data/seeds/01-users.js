31d613815946ec41ee0f632a996dd8b575db7aa5
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-13 19:34:55 -0600

    added desc comment

---
+++ api/data/seeds/01-users.js
@@ -0,0 +1,7 @@
+/*
+ * Author : Stephen Aranda 
+ * File   : 01-users.js
+ * Desc   : file that contains the seed information for dummy users that will be added to db.
+ */
+
+const bcrypt = require('bcrypt');
