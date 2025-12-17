b57fedfe70135e3aaca9bae46e165878049c2018
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-16 21:03:32 -0600

    added desc comments and router

---
+++ api/api-router.js
@@ -0,0 +1,11 @@
+/*
+ * Author : Stephen Aranda
+ * file   : api-router.js
+ * Desc   : Contains router that mounts all sub-routers.js
+ */
+
+const router = require('express').Router();
+
+const authRouter = require('./auth/auth-router');
+
+// TODO-add other routers when completed
