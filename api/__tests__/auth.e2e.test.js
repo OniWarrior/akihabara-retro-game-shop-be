075466c6abf96e249752776557f221e6e2eaffed
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-19 18:27:35 -0600

    added desc comment for test file

---
+++ api/__tests__/auth.e2e.test.js
@@ -0,0 +1,6 @@
+/*
+ * Author: Stephen Aranda
+ * File  : auth.e2e.test.js
+ * Desc  : File that contains all the test that will be conducted involving auth endpoints from
+ *       : auth-router.js
+ */
