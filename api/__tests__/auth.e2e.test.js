29eb71ef3d97210c81837932f7d4342089dfdc87
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-19 18:30:02 -0600

    added imports for tests

--- api/__tests__/auth.e2e.test.js
+++ api/__tests__/auth.e2e.test.js
@@ -4,3 +4,12 @@
  * Desc  : File that contains all the test that will be conducted involving auth endpoints from
  *       : auth-router.js
  */
+
+// import super test for integration test
+const request = require("supertest");
+
+// import Express server app
+const server = require("../server");
+
+// import the database knex instance
+const db = require("../data/dbConfig"); 
