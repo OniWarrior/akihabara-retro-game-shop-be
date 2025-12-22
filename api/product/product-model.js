36968f685195f7c318cc17cb860a22d0837fb5f7
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-21 22:20:14 -0600

    added query to get all available products

--- api/product/product-model.js
+++ api/product/product-model.js
@@ -4,3 +4,25 @@
  * Desc   : Queries for product-router endpoints
  */
 const db = require('../data/dbConfig');
+
+
+/*
+ * getProducts: query to get all available products from db
+ */
+const getProducts = async () => {
+    const products = await db('products')
+        .select(
+            'product_id',
+            'name',
+            'cost',
+            'image_url',
+            'quantity',
+            'product_type',
+            'product_desc'
+        )
+    return products
+}
+
+module.exports = {
+    getProducts
+}
