78878481fc8e69d50fd62d5f880ade1e6fba76ce
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-21 22:14:21 -0600

    added failure response

--- api/product/product-router.js
+++ api/product/product-router.js
@@ -5,3 +5,15 @@
  * 
  */
 const router = require('express').Router();
+
+/*
+ * /products: Endpoint that retrieves all available products for purchase in store
+ */
+router('/products', async (req, res) => {
+    try {
+
+    } catch (err) {
+        // failure response
+        return res.status(500).json({ message: `Server Error: ${err.message}` });
+    }
+})
