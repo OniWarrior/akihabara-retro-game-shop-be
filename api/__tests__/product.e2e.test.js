aaf624bee1b903e633f38afc9481e033ab2bcf4a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-21 22:26:15 -0600

    added imports for testing

--- api/__tests__/product.e2e.test.js
+++ api/__tests__/product.e2e.test.js
@@ -3,3 +3,28 @@
  * file   : product.e2e.test.js
  * Desc   : Integration test for testing the product-router endpoints.
  */
+
+// import super test for integration test
+const request = require("supertest");
+
+// import Express server app
+const server = require("../server.js");
+
+// import the database knex instance
+const db = require("../data/dbConfig.js");
+
+beforeAll(async () => {
+
+    // Ensure schema is up to date in testing db
+    await db.migrate.latest();
+})
+
+beforeEach(async () => {
+    // Clean tables between tests (order matters due to FK constraints if any)
+    await db("session").del();
+    await db("users").del();
+});
+
+afterAll(async () => {
+    await db.destroy();
+});
