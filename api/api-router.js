2775f605c7e12efa65c155c09573d5d149712d93
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-22 20:49:42 -0600

    mounted product router

--- api/api-router.js
+++ api/api-router.js
@@ -8,9 +8,14 @@ const router = require('express').Router();
 
 const authRouter = require('./auth/auth-router');
 
+const productRouter = require('./product/product-router');
+
 // TODO-add other routers when completed
 
 // mount auth router
 router.use('/auth', authRouter);
+
+// mount product router
+router.use('/product', productRouter);
 
 module.exports = router;
