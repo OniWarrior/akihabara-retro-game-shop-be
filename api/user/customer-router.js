496f7e090660af032ee06fd53ab21eaf7f4496b6
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-22 21:45:33 -0600

    added order obj

--- api/user/customer-router.js
+++ api/user/customer-router.js
@@ -17,11 +17,40 @@ router.post('/buy-product/:product_id', async (req, res) => {
         // get the product id
         const { product_id } = req.params;
 
+        // get the quantity
+        const { quantity } = req.body
+
         // get the user id
         const { user_id } = req.session.user;
 
         // retrieve the specific product from db
         const product = await Product.getSpecificProduct(product_id);
+
+        // get the current date
+        const date = new Date()
+
+        // get the day
+        const day = date.getDate();
+
+        // get the month
+        const month = date.getMonth() + 1;
+
+        // get the year
+        const year = date.getFullYear()
+
+        // format the date
+        const formattedDate = `${day}` + "-" + `${month}` + "-" + `${year}`;
+
+        // make the order object
+        const order = {
+            product_id: product.product_id,
+            user_id: user_id,
+            product_name: product.name,
+            cost: product.cost,
+            image_url: product.image_url,
+            quantity: quantity,
+            date: formattedDate
+        }
 
 
 
