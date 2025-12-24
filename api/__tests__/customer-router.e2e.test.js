5c955fb651edc811985cf125a8b6255a59500e9a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-23 22:06:18 -0600

    added expected check on status

--- api/__tests__/customer-router.e2e.test.js
+++ api/__tests__/customer-router.e2e.test.js
@@ -55,6 +55,7 @@ async function getCsrf(agent) {
 // Integration test to test all features of a Customer
 describe("Customer functionality ", () => {
 
+    // happy path for buying an item as a customer
     test("POST Signup -> Login - > Buy item - happy path", async () => {
 
         // get the agent
@@ -127,6 +128,239 @@ describe("Customer functionality ", () => {
 
 
     })
+
+    // sad path for buying an item as a customer-incorrect token
+    test("POST Signup -> Login - > Buy item - sad path", async () => {
+
+        // get the agent
+        const agent = request.agent(server);
+
+        // 1) Signup (POST needs CSRF because you mounted requiredCSRF on /api)
+        const csrf1 = await getCsrf(agent);
+
+        const user = { username: "stephen", password: "Password123!", user_type: "Customer" }
+        // hit the signup endpoint, set csrf token
+        const signup = await agent
+            .post("/api/auth/signup")
+            .set("X-CSRF-Token", csrf1)
+            .set('Content-Type', 'application/json')
+            .send(user);
+
+
+
+        // expect  201 status code on signup completion
+        expect(signup.status).toBe(201);
+
+        // Verify user inserted and password hashed (not plaintext)
+        const userRow = await db("users").where({ username: "stephen" }).first();
+        expect(userRow).toBeTruthy();
+        expect(userRow.password).not.toBe("Password123!");
+
+        // 2) Login (POST also needs CSRF)
+        const csrf2 = await getCsrf(agent);
+
+        const log = { username: "stephen", password: "Password123!" }
+
+        // hit the login endpoint and set teh csrf token
+        const login = await agent
+            .post("/api/auth/login")
+            .set("X-CSRF-Token", csrf2)
+            .set('Content-Type', 'application/json')
+            .send(log);
+
+
+
+        // expect status code to be 200 after success response
+        expect(login.status).toBe(200);
+
+        // 3) Status should now show authenticated
+        const status = await agent.get("/api/auth/status");
+
+        expect(status.status).toBe(200);
+        expect(status.body.authenticated).toBe(true);
+
+        // Your session.user shape might be {user_id, username, user_type}
+        expect(status.body.user.username).toBe("stephen");
+
+
+
+        const product_id = 1;
+        const quantity = 1;
+
+
+
+        // now attempt to buy an item
+        const buyItem = await agent
+            .post(`/api/user/buy-product/${product_id}/${quantity}`)
+            .set("X-CSRF-Token", csrf2)
+            .set('Content-Type', 'application/json')
+
+
+        // check status
+        expect(buyItem.status).toBe(403);
+
+
+
+    })
+
+    // sad path for buying an item as a customer- incorrect req.params
+    test("POST Signup -> Login - > Buy item - sad path", async () => {
+
+        // get the agent
+        const agent = request.agent(server);
+
+        // 1) Signup (POST needs CSRF because you mounted requiredCSRF on /api)
+        const csrf1 = await getCsrf(agent);
+
+        const user = { username: "stephen", password: "Password123!", user_type: "Customer" }
+        // hit the signup endpoint, set csrf token
+        const signup = await agent
+            .post("/api/auth/signup")
+            .set("X-CSRF-Token", csrf1)
+            .set('Content-Type', 'application/json')
+            .send(user);
+
+
+
+        // expect  201 status code on signup completion
+        expect(signup.status).toBe(201);
+
+        // Verify user inserted and password hashed (not plaintext)
+        const userRow = await db("users").where({ username: "stephen" }).first();
+        expect(userRow).toBeTruthy();
+        expect(userRow.password).not.toBe("Password123!");
+
+        // 2) Login (POST also needs CSRF)
+        const csrf2 = await getCsrf(agent);
+
+        const log = { username: "stephen", password: "Password123!" }
+
+        // hit the login endpoint and set teh csrf token
+        const login = await agent
+            .post("/api/auth/login")
+            .set("X-CSRF-Token", csrf2)
+            .set('Content-Type', 'application/json')
+            .send(log);
+
+
+
+        // expect status code to be 200 after success response
+        expect(login.status).toBe(200);
+
+        // 3) Status should now show authenticated
+        const status = await agent.get("/api/auth/status");
+
+        expect(status.status).toBe(200);
+        expect(status.body.authenticated).toBe(true);
+
+        // Your session.user shape might be {user_id, username, user_type}
+        expect(status.body.user.username).toBe("stephen");
+
+
+
+        const product_id = -1;
+        const quantity = -1;
+
+        const csrf3 = await getCsrf(agent)
+
+        // now attempt to buy an item
+        const buyItem = await agent
+            .post(`/api/user/buy-product/${product_id}/${quantity}`)
+            .set("X-CSRF-Token", csrf3)
+            .set('Content-Type', 'application/json')
+
+
+        // check status
+        expect(buyItem.status).toBe(500);
+
+
+
+    })
+
+
+    // happy path for retrieving past orders as a customer
+    test("GET Signup -> Login - > Buy item - > past-orders - happy path", async () => {
+
+        // get the agent
+        const agent = request.agent(server);
+
+        // 1) Signup (POST needs CSRF because you mounted requiredCSRF on /api)
+        const csrf1 = await getCsrf(agent);
+
+        const user = { username: "stephen", password: "Password123!", user_type: "Customer" }
+        // hit the signup endpoint, set csrf token
+        const signup = await agent
+            .post("/api/auth/signup")
+            .set("X-CSRF-Token", csrf1)
+            .set('Content-Type', 'application/json')
+            .send(user);
+
+
+
+        // expect  201 status code on signup completion
+        expect(signup.status).toBe(201);
+
+        // Verify user inserted and password hashed (not plaintext)
+        const userRow = await db("users").where({ username: "stephen" }).first();
+        expect(userRow).toBeTruthy();
+        expect(userRow.password).not.toBe("Password123!");
+
+        // 2) Login (POST also needs CSRF)
+        const csrf2 = await getCsrf(agent);
+
+        const log = { username: "stephen", password: "Password123!" }
+
+        // hit the login endpoint and set teh csrf token
+        const login = await agent
+            .post("/api/auth/login")
+            .set("X-CSRF-Token", csrf2)
+            .set('Content-Type', 'application/json')
+            .send(log);
+
+
+
+        // expect status code to be 200 after success response
+        expect(login.status).toBe(200);
+
+        // 3) Status should now show authenticated
+        const status = await agent.get("/api/auth/status");
+
+        expect(status.status).toBe(200);
+        expect(status.body.authenticated).toBe(true);
+
+        // Your session.user shape might be {user_id, username, user_type}
+        expect(status.body.user.username).toBe("stephen");
+
+
+
+        const product_id = 1;
+        const quantity = 1;
+
+        const csrf3 = await getCsrf(agent);
+
+        // now attempt to buy an item
+        const buyItem = await agent
+            .post(`/api/user/buy-product/${product_id}/${quantity}`)
+            .set("X-CSRF-Token", csrf3)
+            .set('Content-Type', 'application/json')
+
+
+        // check status
+        expect(buyItem.status).toBe(201);
+
+        const pastOrders = await agent
+            .get('/api/user/get-past-orders')
+            .set("Content-Type", "application/json")
+
+        expect(pastOrders.status).toBe(200)
+
+
+
+    })
+
+
+
+
 
 })
 
