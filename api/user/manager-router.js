f22790035bde1d265d740eedd445143108378f13
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-24 19:48:01 -0600

    exported router

--- api/user/manager-router.js
+++ api/user/manager-router.js
@@ -5,12 +5,13 @@
  */
 const Manager = require('./manager-model');
 const router = require('express').Router();
+const { getRole, checkForMissingProduct } = require('./manager-middleware');
 
 /*
  * add-product: endpoint that allows the manager to add a product
  */
 
-router.post('add-product', async (req, res) => {
+router.post('add-product', getRole, checkForMissingProduct, async (req, res) => {
     try {
 
         // deconstruct the req.body to retrieve sent information.
@@ -48,3 +49,6 @@ router.post('add-product', async (req, res) => {
         return res.status(500).json({ message: `Server Error: ${err.message}` });
     }
 })
+
+
+module.exports = router;
